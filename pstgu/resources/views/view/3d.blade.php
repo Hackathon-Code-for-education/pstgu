6e9c6b48376bf58c187c1f133aec7b7c06a83c0a
Daniil Dybka <danil.dybko@gmail.com>
2024-04-20 17:51:42 +0300

    work panorama

--- pstgu/resources/views/view/3d.blade.php
+++ pstgu/resources/views/view/3d.blade.php
@@ -1,1 +1,28 @@
-3d
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A simple example</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css"/>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js"></script>
+    <style>
+    #panorama {
+        width: 100%;
+        height: 95vh;
+    }
+    </style>
+</head>
+<body>
+
+<div id="panorama"></div>
+
+<script>
+pannellum.viewer('panorama', {
+    "type": "equirectangular",
+    "panorama": "{{ asset("img/view/$image") }}"
+});
+</script>
+
+</body>
+</html>
